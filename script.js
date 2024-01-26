e9f7910a71ebc21d02187faf62abbf24aecfdfcc
HectorDanielAyarachiFuentes <135895299+HectorDanielAyarachiFuentes@users.noreply.github.com>
2024-01-26 15:31:15 -0300

    123

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
-var date = new Date();
-document.getElementById("curr-year").innerHTML = date.getFullYear();
+    var date = new Date();
+    var options = { year: 'numeric', month: 'long', day: 'numeric' };
+    document.getElementById("current-date").innerHTML = date.toLocaleDateString('es-ES', options);
